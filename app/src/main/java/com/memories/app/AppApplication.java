df5f9bc4806fcce3622f7c6d92b4f822baed0df3
HamidIdifi <114613662+HamidIdifi@users.noreply.github.com>
2022-10-01 15:35:07 +0200

    adding comment

--- app/src/main/java/com/memories/app/AppApplication.java
+++ app/src/main/java/com/memories/app/AppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AppApplication {
-
+	// hamid
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
