97191ebdaa377e57e8fa04e96a55a5835016416c
noureddine <noureddinelachgar9@gmail.com>
2022-10-03 00:18:23 +0100

    define Exceptions and handle it using controllerAdvice

---
+++ app/src/main/java/com/memories/app/exception/handler/ErrorResponse.java
@@ -0,0 +1,20 @@
+package com.memories.app.exception.handler;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ErrorResponse {
+
+    private int code;
+    private HttpStatus status;
+    private String message;
+
+}
